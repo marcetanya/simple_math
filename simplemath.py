bcf7d410b243a3ba0726e34753b616e713b3cf07
Tanya Llanas <tanya.marcela.llanas@gmail.com>
2019-06-20 11:33:16 -0500

    Added docstrings

--- simplemath.py
+++ simplemath.py
@@ -1,9 +1,18 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+"""SimpleMaath to do basic math
+
+Author: Tanya Llanas
+Copyright 2019
+"""
 
 def add(a, b):
+    "Add two numbers"
     return a + b
+
+def main():
+    "Execute script"
+    print(add(2, 2))
 
 if __name__ == '__main__':
     main()
-print(add(2, 2))
